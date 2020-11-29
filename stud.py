221bf45c9892e355f416ce0f96b92f4b45fc4e5f
Olek Buzunov <oleksandr.buzunov@orthoclinicaldiagnostics.com>
2020-11-28 23:27:58 -0500

    new

--- stud.py
+++ stud.py
@@ -23,27 +23,10 @@ if not home or not home.strip('.'):
     home = os.path.dirname(os.path.abspath(__file__))
 
 
-INSTA_USER  = os.environ.get('INSTA_USER')
-INSTA_PWD 	= os.environ.get('INSTA_PWD')
-
-#assert INSTA_USER
-#assert INSTA_PWD
-
-import traceback
-try:
-    import cStringIO
-except ImportError:
-    import io as cStringIO
-    
-
-if 0:
-    username = INSTA_USER.strip("'").strip('"')
-    passwd = INSTA_PWD.strip("'").strip('"')
-    
 driverpath = r".\driver\nt\chromedriver_86.exe"
 
 phototext = """
-#finalupload
+#ifinite swipe right for Tinder
 """
 
 options = Options()
@@ -57,104 +40,7 @@ options.add_argument("--mute-audio")
 #options.add_experimental_option("mobileEmulation", mobile_emulation)
 options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
 
-def login(driver):
-    driver.get("https://www.instagram.com/accounts/login/?hl=en")
-    time.sleep(3)
-    driver.find_element_by_xpath("//*[@id='loginForm']/div[1]/div[3]/div/label/input").send_keys(username)
-    time.sleep(0.5)
-    driver.find_element_by_xpath("//*[@id='loginForm']/div[1]/div[4]/div/label/input").send_keys(passwd)
-    time.sleep(0.5)
-    driver.find_element_by_xpath("//*[@id='loginForm']/div[1]/div[6]/button/div").click()
-    time.sleep(3)
-    save_login= None
-    #e()
-    if 1: 
-        try:
-            print('--------get app-------------------')
-            driver.find_element_by_xpath("//*[@id='react-root']/div/div[2]/a[2]").click()
-        except:
-            err_log = cStringIO.StringIO()
-            traceback.print_exc(file=err_log)
-            err = err_log.getvalue()
-            print(err)
-        
-        
-        
-    if 0:
-        try:
-            save_login = driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/section/div/div[2]")
-            pp(dir(save_login))
-        except:
-            err_log = cStringIO.StringIO()
-            traceback.print_exc(file=err_log)
-            err = err_log.getvalue()
-            print(err)
-    if 0:
-        if save_login:
-            sli = 'Save Your Login Info?'
-            if save_login.text == sli:
-                #Not now
-                 driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/button').click()
-            else:
-                raise Exception('Expecting "%s", got "%s".' % (sli, save_login.text))
-    #Add Instagram to your Home screen?
-    try:
-        print('--------Cancel-------------------')
-        time.sleep(2)
-        driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[3]/button[2]').click()
-        
-        #body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm
-        #<button class="aOOlW   HoLwm " tabindex="0">Cancel</button>
-        #driver.findElementByClassName("aOOlW   HoLwm ").click();
-    except:
-        err_log = cStringIO.StringIO()
-        traceback.print_exc(file=err_log)
-        err = err_log.getvalue()
-        print(err)
-    try:
-        time.sleep(1)
-        driver.find_element_by_xpath("//*[@id='react-root']/div/div[2]/a[2]").click()
-    except:
-        err_log = cStringIO.StringIO()
-        traceback.print_exc(file=err_log)
-        err = err_log.getvalue()
-        print(err)
-    #driver.find_element_by_xpath("//*[@id='react-root']/section/main/article/div/div/div/form/div/div[2]/a[@title = 'Not Now']").click()
-    #//*[@id="react-root"]/div/div[2]/a[2
-    try:
-        time.sleep(1)
-        driver.find_element_by_xpath("/html/body/div[2]/div/div/div[3]/button[2]").click()
-    except:
-        err_log = cStringIO.StringIO()
-        traceback.print_exc(file=err_log)
-        err = err_log.getvalue()
-        print(err)
-def upload(driver,phototext, photopath):
 
-    driver.find_element_by_xpath("//div[@role='menuitem']").click()
-    time.sleep(1.5)
-    
-    autoit.win_active("Open") #open can change by your os language if not open change that
-    time.sleep(2)
-    autoit.control_send("Open", "Edit1", photopath)
-    time.sleep(1.5)
-    autoit.control_send("Open", "Edit1", "{ENTER}")
-    time.sleep(2)
-    driver.find_element_by_xpath("//*[@id='react-root']/section/div[1]/header/div/div[2]/button").click()
-    time.sleep(1)
-    
-    ta= driver.find_elements(By.XPATH, '//textarea')
-    
-    for part in phototext.split('\n'):
-        pp (part)
-        if part:
-            ta[0].send_keys(part)
-            ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(Keys.SHIFT).key_up(Keys.ENTER).perform()
-        else:
-            pass
-    time.sleep(1)
-        
-    driver.find_element_by_xpath("//*[@id='react-root']/section/div[1]/header/div/div[2]/button").click()
 if __name__=="__main__":
     #//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button/span/svg/path
     if 1:
@@ -162,16 +48,33 @@ if __name__=="__main__":
         driver.get("https://tinder.com/")
         
         time.sleep(1)
-        driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button').click()
-        time.sleep(1)
-        driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[3]/button/span[2]').click()
-        time.sleep(1)
-        phone=driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div[2]/div/input')
-        phone.click()
-        time.sleep(.2)
-        phone.send_keys("2125183000")
+        driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/button').click()
         time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/button/span').click()
+        if 0:
+            driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[3]/button/span[2]').click()
+            time.sleep(1)
+            phone=driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div[2]/div/input')
+            phone.click()
+            time.sleep(.2)
+            phone.send_keys("2125183000")
+            time.sleep(2)
+            driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[1]/div/button/span[2]').click()
+        
+        driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[1]/div/button').click()
+        time.sleep(2)
+        if 0:
+            email=driver.find_element_by_xpath('//*[@id="identifierId"]')
+            email.click()
+            time.sleep(.2)
+            email.send_keys("olek.buzu@gmail.com")
+        if 0:
+            email=driver.find_elements_by_class_name("whsOnd zHQkBf");
+            print(email)
+            print(dir(email[0]))
+            email.click()
+            time.sleep(.2)
+            email.send_keys("olek.buzu@gmail.com")
+        #time.sleep(2)
         go = input("Continue?")
         if go=='y':
             pass
@@ -196,11 +99,11 @@ if __name__=="__main__":
         time.sleep(1)
         while True:
             try:
-                driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/div/main/div/div[1]/div/div[2]/div[4]/button').click()
+                driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[4]/button').click()
                                               
             except:
                 try:
-                    driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button').click()
+                    driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/div/main/div/div[1]/div/div[2]/div[4]/button').click()
                                                   
                 except:
 
@@ -209,37 +112,47 @@ if __name__=="__main__":
                     except:
                         print('Passing on "Upgrade Your Like".')
                         try:
-                            driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]/span').click()
+                            driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/button[2]').click()
                         except:
                             print('Passing on "Add Tinder to your Home Screen".')
                             secs = randrange(10) +1
                             print('Restarting in', secs)
                             time.sleep(randrange(10))
-            secs = randrange(60) +5*randrange(10)
+            secs = randrange(5,10) +randrange(2,10)
             print('Sleep for ', secs)
             time.sleep(secs)
-
-    if 0:
-        driver.close()
-    if 0:
-        login(driver)
-
-        photopath = os.path.join(home, r"images\DSC02225.JPG" )
-        upload(driver,phototext, photopath)
-    if 0:
-        #Turn on notifications
-        try:
-            
-            #Not now 
-            time.sleep(5)
-            driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
-            #time.sleep(10)
-            print('Done.')
-        except:
-            err_log = cStringIO.StringIO()
-            traceback.print_exc(file=err_log)
-            err = err_log.getvalue()
-            print(err)
-    
-    if 0:
-        driver.close()
+            if 1: # get name
+                span=driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[3]/div/div[1]/div/div[1]/span')
+                print('Name:', span.text)
+            tid=2
+            tabs={2:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[2]',
+            3:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[3]',
+            4:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[4]',
+            5:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[5]',
+            6:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[6]',
+            7:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[7]',
+            8:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[8]',
+            9:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[9]',
+            10:'//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[2]/button[10]'}
+            while True:
+                try:
+                    print('Trying left: %d' % tid)
+                    driver.find_element_by_xpath(tabs[tid]).click()
+                    print('Left ok: %d' % tid)
+                    try: #try video
+                        
+                        driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/span[3]/div[2]/div').click()
+                    except:
+                        print('Not a video: %d' % tid)
+                    tid +=1
+                    time.sleep(5)
+                except:
+                    print('Pass on left: %d' % tid)
+                    break
+            time.sleep(.5)
+            if 0:
+                go = input("Continue?")
+                if go=='y':
+                    pass
+                else:
+                    exit(1)
